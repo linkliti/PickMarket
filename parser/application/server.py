e648dfe64bb957d94fe40847c0f0d59d50b2b475
linkliti <popovalex1100@gmail.com>
2024-02-06 22:57:20 +0300

    py: categories recursion

--- parser/application/server.py
+++ parser/application/server.py
@@ -1,11 +1,37 @@
+from bs4 import BeautifulSoup
 from seleniumbase import Driver
 from seleniumbase import undetected
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def get_categories(driver: undetected.Chrome, url: str, selector: str) -> list:
+  driver.default_get(url)
+  html = driver.page_source
+  soup = BeautifulSoup(html, 'html.parser')
+  categories = soup.select(selector)
+  subcategories = []
+  for category in categories:
+    subcategories += get_categories(
+      driver, "https://www.ozon.ru" + category['href'],
+      '[data-widget="filtersDesktop"] div[style="margin-left:16px;"] a[href*="/category"]'
+    )
+  return [[category.text, "https://www.ozon.ru" + category['href']] for category in categories + subcategories]
+
 
 
 def server():
-  driver: undetected.Chrome = Driver(uc=True)
+  caps = DesiredCapabilities().CHROME
+  caps["pageLoadStrategy"] = "none"
+  driver: undetected.Chrome = Driver(uc=True, headless=False, mobile=False, cap_string=caps)
+  driver.get("https://www.ozon.ru")
   try:
-    driver.get('https://ozon.ru')
-    driver.save_screenshot('nowsecure.png')
+    print(
+      get_categories(driver, 'https://www.ozon.ru/category/',
+                     'a[href*="/category"]'))
+    driver.sleep(10)
   finally:
     driver.quit()
