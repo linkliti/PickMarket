d88a8837c93b8da3613ac06e3460a8b24333e18c
linkliti <popovalex1100@gmail.com>
2024-02-07 00:28:16 +0300

    py: formatting

--- parser/application/server.py
+++ parser/application/server.py
@@ -1,11 +1,6 @@
 from bs4 import BeautifulSoup
 from seleniumbase import Driver
 from seleniumbase import undetected
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import DesiredCapabilities
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import logging
 import sys
 
@@ -14,7 +9,8 @@ def get_categories(driver: undetected.Chrome, url: str, selector: str) -> list:
   logging.debug("getting categories from:" + url)
   driver.default_get(url)
   html = driver.page_source
-  while 'Please, enable JavaScript to continue' not in html:
+  print(html)
+  while 'Please, enable JavaScript to continue' in html:
     logging.debug("anti-bot triggered, refreshing...")
     driver.get(url)
     html = driver.page_source
@@ -23,19 +19,21 @@ def get_categories(driver: undetected.Chrome, url: str, selector: str) -> list:
   subcategories = []
   for category in categories:
     subcategories += get_categories(
-      driver, "https://www.ozon.ru" + category['href'],
-      '[data-widget="filtersDesktop"] div[style="margin-left:16px;"] a[href*="/category"]'
-    )
-  return [[category.text, "https://www.ozon.ru" + category['href']] for category in categories + subcategories]
+      driver=driver,
+      url="https://www.ozon.ru" + category['href'],
+      selector='[data-widget="filtersDesktop"] div[style="margin-left:16px;"]'
+      + ' a[href*="/category"]')
+  return [[category.text, "https://www.ozon.ru" + category['href']]
+          for category in categories + subcategories]
 
 
-
-def server():
+def server() -> None:
   logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
   # chrome
-  caps = DesiredCapabilities().CHROME
-  caps["pageLoadStrategy"] = "eager"
-  driver: undetected.Chrome = Driver(uc=True, headless=True, mobile=False, cap_string=caps)
+  driver: undetected.Chrome = Driver(uc=True,
+                                     headless=False,
+                                     mobile=False,
+                                     cap_file='./capabilities.py')
   # driver.get("https://www.ozon.ru")
   logging.debug("chrome started")
   try:
