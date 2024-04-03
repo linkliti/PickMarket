b963ebf354f1bc71e09bc6eecfa9d414325f132d
linkliti <popovalex1100@gmail.com>
2024-04-03 20:25:11 +0300

    JS: Docker & update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5005,
+    strictPort: true,
+  },
+  preview: {
+    port: 5005,
+    strictPort: true,
+  },
   plugins: [react()],
 })
