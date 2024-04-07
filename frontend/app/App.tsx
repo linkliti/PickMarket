37461469b4c6bf53641f4990981cd971d657bce4
linkliti <popovalex1100@gmail.com>
2024-04-07 15:35:01 +0300

    JS: Better runtime error overlay

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -161,6 +161,23 @@ function Base() {
 }
 
 function App() {
+  if (import.meta.env.DEV) {
+    window.onerror = (_event, _source, _lineno, _colno, err) => {
+      // must be within function call because that's when the element is defined for sure.
+      const ErrorOverlay = customElements.get("vite-error-overlay");
+      // don't open outside vite environment
+      if (!ErrorOverlay) {
+        return;
+      }
+      if (!err) {
+        err = new Error("Unknown error");
+      }
+      err.message = "Runtime Error: " + err.message;
+      const overlay = new ErrorOverlay(err);
+      document.body.appendChild(overlay);
+    };
+  }
+
   return (
     <div className="min-h-screen flex flex-col items-center px-16 pt-10 pb-20 bg-sky-100 max-md:px-5">
       <div className="flex flex-col w-full max-w-[1065px] max-md:max-w-full">
