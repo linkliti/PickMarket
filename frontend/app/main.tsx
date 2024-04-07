d028b1ebd4eba243ff631a39a4238169c026aff2
linkliti <popovalex1100@gmail.com>
2024-04-07 16:05:53 +0300

    JS: Routing

--- frontend/app/main.tsx
+++ frontend/app/main.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./App";
+import Base from "./Base";
+import NotFound from "./components/base/404";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+      {
+        path: "/",
+        element: <Base />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
